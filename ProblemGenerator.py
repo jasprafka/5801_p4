f4bbb170092a28e69ce733cfc15a2a2c9ca76a27
Jake Sprafka <jasprafka@gmail.com>
2022-11-17 21:01:04 -0600

    Initial Commit

---
+++ ProblemGenerator.py
@@ -0,0 +1,10 @@
+'''
+ProblemGenerator class as described in the PPALMS design document.
+'''
+
+
+class ProblemGenerator():
+    """ProblemGenerator class as described in the PPALMS design document."""
+    
+    def __init__(self) -> None:
+        pass
---
+++ ProblemGeneratorTest.py
@@ -0,0 +1,9 @@
+import unittest
+import ProblemGenerator
+
+class ProblemGeneratorTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
