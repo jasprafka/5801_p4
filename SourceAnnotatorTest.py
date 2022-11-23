f3245ca808899d3501b025b863ce1e96e3d78b5e
Jake Sprafka <jasprafka@gmail.com>
2022-11-23 13:02:15 -0600

    Finished testing for SourceAnnotator.py

--- SourceAnnotatorTest.py
+++ SourceAnnotatorTest.py
@@ -1,45 +1,8 @@
 import unittest
 from unittest.mock import MagicMock
 import SourceAnnotator
+from SourceAnnotatorTestCases import *
 
-_VALID_CPP_PATH = "./test_files/test_code.cpp"
-_VALID_PY_PATH = "./test_files/test_code.py"
-_EMPTY_PY_PATH = "./test_files/test_empty.py"
-_EMPTY_CPP_PATH = "./test_files/test_empty.cpp"
-_COMMENT_PY_PATH = "./test_files/test_only_comments.py"
-_COMMENT_CPP_PATH = "./test_files/test_only_comments.cpp"
-_INVALID_PATH = "./invalid_path.cat"
-
-_CSTYLE_COMMENT_LINES = [
-    "int x = 0;",
-    "x = 0; // comment",
-    "// comment\n",
-    " // comment",
-    "\t// comment",
-    "//// comment \n",
-    " //// comment\t\n",
-    "\t//// comment"
-]
-
-_PYTHONIC_COMMENT_LINES = [
-    "x = 0",
-    "x = 0 # comment",
-    "# comment",
-    " # comment\n",
-    "\t# comment \n",
-    "## comment\n",
-    " ## comment",
-    "\t## comment\t\n"
-]
-
-_COMMENTLESS_LINES = [
-    "int x = 0;",
-    "z = 0",
-    "print('# some string')"
-]
-
-_SELECTED_lINES = [1, 2, 2, 3, 4, 5, 6, 6]
-_LINE_TUPLE = (1, 2, 3, 4, 5, 6)
 
 class SourceAnnotatorTest(unittest.TestCase):
     """TestCase subclass to run unittests on all functions in SourceAnnotator."""
@@ -47,35 +10,75 @@ class SourceAnnotatorTest(unittest.TestCase):
     def setUp(self): # Gets run before each test
         self.src_annotator = SourceAnnotator.SourceAnnotator()
         SourceAnnotator.print = MagicMock()
+        self.rmlc = SourceAnnotator.remove_line_comments
 
     def tearDown(self): # Gets run after each test
+        SourceAnnotator.print = print
+        self.rmlc = None
         pass
     
     def test_upload_file(self):
-        self.assertTrue(self.src_annotator.upload_file(_VALID_CPP_PATH))
-        self.assertTrue(self.src_annotator.upload_file(_VALID_PY_PATH))
-        self.assertFalse(self.src_annotator.upload_file(_EMPTY_PY_PATH))
-        self.assertFalse(self.src_annotator.upload_file(_EMPTY_CPP_PATH))
-        self.assertFalse(self.src_annotator.upload_file(_COMMENT_PY_PATH))
-        self.assertFalse(self.src_annotator.upload_file(_COMMENT_CPP_PATH))
-        self.assertFalse(self.src_annotator.upload_file(_INVALID_PATH))
+        self.assertTrue(self.src_annotator.upload_file(VALID_CPP_PATH))
+        self.assertTrue(self.src_annotator.upload_file(VALID_PY_PATH))
+        self.assertTrue(self.src_annotator.upload_file(EMPTY_PY_PATH))
+        self.assertTrue(self.src_annotator.upload_file(EMPTY_CPP_PATH))
+        self.assertTrue(self.src_annotator.upload_file(COMMENT_PY_PATH))
+        self.assertTrue(self.src_annotator.upload_file(COMMENT_CPP_PATH))
+        self.assertFalse(self.src_annotator.upload_file(INVALID_PATH))
+        self.assertFalse(self.src_annotator.upload_file(NO_PATH))
     
     def test_remove_line_comments(self):
-        self.assertEqual(SourceAnnotator.remove_line_comments(_CSTYLE_COMMENT_LINES, '//'), _CSTYLE_COMMENT_LINES[0:2])
-        self.assertEqual(SourceAnnotator.remove_line_comments(_PYTHONIC_COMMENT_LINES, '#'), _PYTHONIC_COMMENT_LINES[0:2])
-        self.assertEqual(SourceAnnotator.remove_line_comments(_COMMENTLESS_LINES, '#'), _COMMENTLESS_LINES)
-        self.assertEqual(SourceAnnotator.remove_line_comments(_COMMENTLESS_LINES, '//'), _COMMENTLESS_LINES)
+        self.assertEqual(SourceAnnotator.remove_line_comments(CSTYLE_COMMENT_LINES, '//'), CSTYLE_COMMENT_LINES[0:2])
+        self.assertEqual(SourceAnnotator.remove_line_comments(CSTYLE_COMMENT_LINES, None), CSTYLE_COMMENT_LINES)
+        self.assertEqual(SourceAnnotator.remove_line_comments(PYTHONIC_COMMENT_LINES, '#'), PYTHONIC_COMMENT_LINES[0:2])
+        self.assertEqual(SourceAnnotator.remove_line_comments(PYTHONIC_COMMENT_LINES, None), PYTHONIC_COMMENT_LINES)
+        self.assertEqual(SourceAnnotator.remove_line_comments(COMMENTLESS_LINES, '#'), COMMENTLESS_LINES)
+        self.assertEqual(SourceAnnotator.remove_line_comments(COMMENTLESS_LINES, '//'), COMMENTLESS_LINES)
     
+    def test_strip_comments(self):
+
+        SourceAnnotator.remove_line_comments = MagicMock()
+
+        self.src_annotator._file_lines = CSTYLE_COMMENT_LINES
+        self.src_annotator.strip_comments()
+        SourceAnnotator.remove_line_comments.assert_any_call(CSTYLE_COMMENT_LINES, None)
+        
+        self.src_annotator._file_lines = CSTYLE_COMMENT_LINES
+        self.src_annotator._file_type = "c"
+        self.src_annotator.strip_comments()
+        SourceAnnotator.remove_line_comments.assert_any_call(CSTYLE_COMMENT_LINES, '//')
+
+        SourceAnnotator.remove_line_comments = self.rmlc
+
     def test_select_lines(self):
-        lines = self.src_annotator.select_lines(_SELECTED_lINES)
-        self.assertEqual(lines, set(_SELECTED_lINES))
-        self.assertNotEqual(lines, _SELECTED_lINES)
+        SourceAnnotator.len = MagicMock(return_value=10)
+        lines = self.src_annotator.select_lines(SELECTED_lINES)
+        self.assertListEqual(lines, list(set(SELECTED_lINES)))
+        self.assertNotEqual(lines, SELECTED_lINES)
+        SourceAnnotator.len = len
     
     def test_create_tuple(self):
-        self.src_annotator.select_lines(_SELECTED_lINES)
-        self.assertTupleEqual(self.src_annotator.create_tuple(), _LINE_TUPLE)
-        self.assertListEqual(self.src_annotator._line_tuples,[_LINE_TUPLE])
+        SourceAnnotator.tuple = MagicMock(return_value=(LINE_TUPLE_1))
+        self.assertTupleEqual(self.src_annotator.create_tuple(), (LINE_TUPLE_1))
+        self.assertTupleEqual(self.src_annotator._line_tuples[0], (LINE_TUPLE_1))
+        SourceAnnotator.tuple = tuple
+        
+    
+    def test_get_tuples(self):
+        self.assertFalse(self.src_annotator._line_tuples)
+        self.src_annotator._line_tuples = [LINE_TUPLE_1]
+        self.assertListEqual(self.src_annotator.get_tuples(), [LINE_TUPLE_1])
 
+    
+    def test_get_lines(self):
+        self.assertFalse(self.src_annotator._file_lines)
+        self.src_annotator._file_lines = COMMENTLESS_LINES
+        self.assertListEqual(self.src_annotator.get_lines(), COMMENTLESS_LINES)
+    
+    def test_get_selected_lines(self):
+        self.assertFalse(self.src_annotator._selected_lines)
+        self.src_annotator._selected_lines = SELECTED_lINES
+        self.assertListEqual(self.src_annotator.get_selected_lines(), SELECTED_lINES)
 
 
 if __name__ == "__main__":
