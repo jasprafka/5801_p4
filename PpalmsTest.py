1e0acbfc51be963ae82981ce47ce9c00567f9db4
Felix Su <81030517+Swekinng@users.noreply.github.com>
2022-11-30 18:55:58 -0600

    new version of ppalmsTest

--- PpalmsTest.py
+++ PpalmsTest.py
@@ -15,7 +15,8 @@ _FUNCTIONS = {
         "5" : ppalms.select_lines,
         "6" : ppalms.show_selected_lines,
         "7" : ppalms.create_tuple,
-        "8" : ppalms.get_tuples
+        "8" : ppalms.get_tuples,
+        "9" : ppalms.main,
 }
 
 _MAIN_MENU = (
@@ -38,7 +39,16 @@ class PpalmsTest(unittest.TestCase):
         SourceAnnotator.print = print
         pass
         
+    # We will not test the main() function in PpalmsTest since the only change is the path
+    # of the upload file in the commend, and tihe test of upload file path will be test in
+    # SourceAnnotatorTest
     
+    # Every test in PpalmsTest will be running with the valid file path since the program 
+    # will not be running with invalid file path  
+    
+    # Every test in PpalmsTest will be running with the valid cpp file, since SourceAnnotatorTest will
+    # detect the different behavior between cpp and py file.
+    	
     def test_exit_ppalms(self):
     	capturedOutput = io.StringIO()
     	sys.stdout = capturedOutput
@@ -76,9 +86,28 @@ class PpalmsTest(unittest.TestCase):
     # But this package "pyautogui" is not installed on our lab machine. 
     # Hence, I comment out this test for now.
     
-    #def test_select_lines(self):
-    	#self.src_annotator.upload_file(VALID_CPP_PATH)
-    	#self.assertEqual(_FUNCTIONS.get("5")(self.src_annotator), False)
+    def test_select_lines(self):
+    
+    	# Test for valid input value
+    	print()
+    	print("please input 1 for this test")
+    	capturedOutput = io.StringIO()
+    	sys.stdout = capturedOutput
+    	self.src_annotator.upload_file(VALID_CPP_PATH)
+    	self.assertEqual(_FUNCTIONS.get("5")(self.src_annotator), False)
+    	sys.stdout = sys.__stdout__
+    	self.assertEqual(capturedOutput.getvalue(), '\n0 #include <iostream>\n1 \n2 // Using namespace std is bad practice\n3 using namespace std;\n4 \n5 int main() {\n6     // Variable declarations\n7     int x = 0;\n8     int y = 0;\n9     int z = 0;\n10 \n11     // Variables must be declared before they are used\n12     cout << "x = " << x << endl;\n13     cout << "y = " << x << endl;\n14     cout << "z = " << x << endl;\n15 \n16     return 0;\n17 }\nEnter your selection as line numbers separated by spaces\ne.g. - \n1 2 5 7 10\n\nYour line selection: 	-- Selected Lines: [1]\n')
+    	
+    	# Test for invalid input value
+    	print()
+    	print("please input 100 for this test")
+    	capturedOutput = io.StringIO()
+    	sys.stdout = capturedOutput
+    	self.src_annotator.upload_file(VALID_CPP_PATH)
+    	self.assertEqual(_FUNCTIONS.get("5")(self.src_annotator), False)
+    	sys.stdout = sys.__stdout__
+    	self.assertEqual(capturedOutput.getvalue(), '\n0 #include <iostream>\n1 \n2 // Using namespace std is bad practice\n3 using namespace std;\n4 \n5 int main() {\n6     // Variable declarations\n7     int x = 0;\n8     int y = 0;\n9     int z = 0;\n10 \n11     // Variables must be declared before they are used\n12     cout << "x = " << x << endl;\n13     cout << "y = " << x << endl;\n14     cout << "z = " << x << endl;\n15 \n16     return 0;\n17 }\nEnter your selection as line numbers separated by spaces\ne.g. - \n1 2 5 7 10\n\nYour line selection: 	-- No lines selected!\n')
+    	
     	
     def test_show_selected_lines(self):
     	capturedOutput = io.StringIO()
